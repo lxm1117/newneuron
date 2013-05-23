c972c874f6cc7049496acfae4fbc03413d94411e
lxm1117 <lxm1117@gmail.com>
2013-05-23 10:38:57 -0400

    second commit

---
+++ i686/rxd_figure.py
@@ -0,0 +1,7 @@
+g.addvar("ca1", ca.nodes[1]._ref_concentration)
+g.addvar("calm1", calm.nodes[1]._ref_concentration)
+g.addvar("ca-calm1", cacalm.nodes[1]._ref_concentration)	
+g.addvar("ca2", ca.nodes[101]._ref_concentration)
+g.addvar("calm2", calm.nodes[101]._ref_concentration)
+g.addvar("ca-calm2", cacalm.nodes[101]._ref_concentration)	
+
